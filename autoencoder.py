db1ca79091f483ae00a8c1242dcdcf1b7f9aee29
Andrew Seagraves <aseagraves@andrews-air.attlocal.net>
2020-01-02 23:49:09 -0600

    implemented a sliding window with overlap on the left and right

--- autoencoder.py
+++ autoencoder.py
@@ -1,7 +1,9 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-def build_model(model, layers):
+def build_model(layers):
+    model = Sequential()
+    
     for layer in layers[:-1]:
         model.add(Dense(layer, activation='relu'))
     model.add(Dense(layers[-1], activation='linear'))
--- driver.py
+++ driver.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
- Driver for autoencoder training hyperparameter studies
+ Driver for hyperparameter and architecture grid search
 """
 from train import train
 import os
@@ -12,44 +12,58 @@ if not os.path.exists(base_dir):
     os.makedirs(base_dir)
 
 # Root data directory
-root_dir = "../data/LibriSpeech/dev-clean"
+root_dir = '../data/LibriSpeech/dev-clean'
+
+# Speech dict file
+speech_dict_file = root_dir + '/speech_dict.pkl'
 
 # Hyperparameters
-params_master = [[0.05, 16000, 128, 100, 0.2, 1e-4, False],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True],
-                [0.05, 16000, 128, 100, 0.2, 1e-4, True]]
+params_master = [[0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, False],
+                 [0.05, 0.01, 0.01, 16000, 128, 100, 0.2, 1e-4, True],
+                 [0.05, 0.025, 0.025, 16000, 128, 100, 0.2, 1e-4, True]]
 
 # Size reduction factors for neural net layers
-layer_params = [[1, 4, 1],
-                [1, 8, 1],
-                [1, 16, 1],
-                [1, 2, 4, 2, 1],
-                [1, 4, 8, 4, 1],
-                [1, 4, 16, 4, 1],
-                [1, 1, 8, 1, 1],
+layer_params = [[1, 1, 16, 1, 1],
                 [1, 1, 16, 1, 1],
-                [1, 1, 1, 8, 1, 1, 1],
-                [1, 1, 1, 16, 1, 1, 1],
-                [1, 1, 1, 32, 1, 1, 1]]
+                [1, 1, 16, 1, 1]]
+
+
+# params_master = [[0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, False],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True],
+#                 [0.05, 0.0, 0.0, 16000, 128, 100, 0.2, 1e-4, True]]
+
+# layer_params = [[1, 4, 1],
+#                 [1, 8, 1],
+#                 [1, 16, 1],
+#                 [1, 2, 4, 2, 1],
+#                 [1, 4, 8, 4, 1],
+#                 [1, 4, 16, 4, 1],
+#                 [1, 1, 8, 1, 1],
+#                 [1, 1, 16, 1, 1],
+#                 [1, 1, 1, 8, 1, 1, 1],
+#                 [1, 1, 1, 16, 1, 1, 1],
+#                 [1, 1, 1, 32, 1, 1, 1]]
 
 # Carry out training parameter studies
 for i, params in enumerate(params_master):
     hyperparams = {'dt_chunk': params[0],
-                   'sr': params[1],
-                   'batch_size': params[2],
-                   'epochs': params[3],
-                   'min_val_frac': params[4],
-                   'lr': params[5],
-                   'read_partitions': params[6]}
+                   'dt_lwin': params[1],
+                   'dt_rwin': params[2],
+                   'sr': params[3],
+                   'batch_size': params[4],
+                   'epochs': params[5],
+                   'min_val_frac': params[6],
+                   'lr': params[7],
+                   'read_partitions': params[8]}
 
-    out_dir = base_dir + '/net' + str(i)
-    train(root_dir, out_dir, hyperparams, layer_params[i])
+    out_dir = base_dir + '/test' + str(i)
+    train(root_dir, out_dir, speech_dict_file, hyperparams, layer_params[i])
 
